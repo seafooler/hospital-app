6943f90eca6099d41ca6594c86e739fc62e2cd50
seafooler <seafooler@gmail.com>
2017-12-05 22:01:07 +0800

    The 2nd temp commit of apk installation, before leaving the lab

--- app/src/main/java/com/example/seafooler/hospitalapp/SDCardFileExplorerActivity.java
+++ app/src/main/java/com/example/seafooler/hospitalapp/SDCardFileExplorerActivity.java
@@ -2,14 +2,22 @@ package com.example.seafooler.hospitalapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class SDCardFileExplorerActivity extends AppCompatActivity {
 
-    private Textview tvPath;
+    private TextView tvPath;
     private ListView lvFiles;
     private Button btnParent;
 
@@ -23,6 +31,53 @@ public class SDCardFileExplorerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sdcard_file_explorer);
-        lvFiles = this.findViewById(R.id.files);
+        lvFiles = (ListView) this.findViewById(R.id.files);
+        tvPath = (TextView) this.findViewById(R.id.tvpath);
+        btnParent = (Button)this.findViewById(R.id.btnParent);
+
+        //获取系统的SDCard的目录
+        File root = new File("/mnt/sdcard");
+
+        //如果SD卡存在的话
+        if(root.exists()) {
+            currentParent = root;
+            currentFiles = root.listFiles();
+            //使用当前目录下的全部文件、文件夹来填充listView
+            inflateListView(currentFiles);
+        }
+
+//        lvFile.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//
+//            }
+//        });
+    }
+
+
+    /**
+     * 根据文件夹填充ListView
+     * @param files
+     */
+    private void inflateListView(File[] files) {
+        List<Map<String, Object>> listItems = new ArrayList<Map<String, Object>>();
+        for (int i=0; i< files.length; i++) {
+            Map<String, Object> listItem = new HashMap<String, Object>();
+            if (files[i].isDirectory()) {
+                //如果是文件夹就显示图片为文件夹的图片
+                listItem.put("icon", R.drawable.folder);
+            } else {
+                listItem.put("icon", R.drawable.file);
+            }
+        }
+    }
+
+    /**
+     * 展现所有的Apk
+     */
+    private class ApkAdapter extends BaseAdapter {
+        public int getCount() {
+            return
+        }
     }
 }
