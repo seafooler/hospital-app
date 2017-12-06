47e71b27b02a23cf9dc4ac10c2e23f40b6cf002c
seafooler <seafooler@gmail.com>
2017-12-07 00:29:13 +0800

    The 5th temp commit of apk installation, before sleep

--- app/src/main/java/com/example/seafooler/hospitalapp/InstallGameActivity.java
+++ app/src/main/java/com/example/seafooler/hospitalapp/InstallGameActivity.java
@@ -30,7 +30,6 @@ import com.example.seafooler.hospitalapp.common.SDCardUtil;
 
 public class InstallGameActivity extends AppCompatActivity {
 
-    private GridView gvFiles;
     private String TAG = "InstallGa meActivity";
     private String gamePath;
     private File[] gameFiles;
@@ -40,9 +39,7 @@ public class InstallGameActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.install_game);
-//        gamePath = createGameDir();
-        gvFiles = this.findViewById(R.id.gamesGridView);
-/*
+        gamePath = createGameDir();
 
         File gameDir = new File("gamePath");
         try {
@@ -54,13 +51,10 @@ public class InstallGameActivity extends AppCompatActivity {
 
         bitmapMaps = inflateBitmapMap(gameFiles);
 
-//        GameAdpater gameGridAdapter = new GameAdpater(getBaseContext(), )
-//
-//        inflateListView(gameFiles);
 
         GridView gridView = findViewById(R.id.gamesGridView);
 
-        gridView.setAdapter(new GameAdpater(this, bms));
+        gridView.setAdapter(new GameAdpater(this, bitmapMaps));
 
         gridView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
             @Override
@@ -86,13 +80,11 @@ public class InstallGameActivity extends AppCompatActivity {
         }
     }
 
-    */
-/**
- * Generate Bitmap arrays from game folder
- * @param gameFiles
- * @return Map<String, Bitmap>
- *//*
-
+    /**
+     * Generate Bitmap arrays from game folder
+     * @param gameFiles
+     * @return Map<String, Bitmap>
+     */
     private Map<String, Bitmap> inflateBitmapMap(File[] gameFiles) {
         Map<String, Bitmap> apkBitmapMap = new ArrayMap<>();
         List<String> bitmapPngNames = new ArrayList<>();
@@ -169,215 +161,8 @@ public class InstallGameActivity extends AppCompatActivity {
 
     }
 
-    for (int i=0; i< files.length; i++) {
-        Map<String, Object> listItem = new HashMap<String, Object>();
-        if (files[i].isDirectory()) {
-            //如果是文件夹就显示图片为文件夹的图片
-            listItem.put("icon", R.drawable.folder);
-        } else {
-            listItem.put("icon", R.drawable.file);
-        }
-        Log.i("SDCard", "2222222");
-
-    */
-/*
-    private void inflateListView(File[] files) {
-        if (files == null) {
-            Log.i(TAG, "Game files is empty");
-            return;
-        }
-
-        List<Map<String, Object>> listItems = new ArrayList<Map<String, Object>>();
-        for (int i=0; i< files.length; i++) {
-            Map<String, Object> listItem = new HashMap<String, Object>();
 
 
-
-            //添加一个文件名称
-            listItem.put("filename", files[i].getName());
-            File myFile = new File(files[i].getName());
-
-            //获取文件的最后修改日期
-            long modTime = myFile.lastModified();
-            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-d HH:mm:ss");
-            System.out.println(dateFormat.format(new Date(modTime)));
-
-            //添加一个最后修改日期
-            listItem.put("modify", "修改日期："+dateFormat.format(new Date(modTime)));
-            listItems.add(listItem);
-        }
-        Log.i("SDCard", "33333333");
-
-        //定义一个SimpleAdapter
-        SimpleAdapter adapter = new SimpleAdapter(
-                InstallGameActivity.this, listItems, R.layout.list_item,
-                new String[]{"filename", "icon", "modify"}, new int[] {R.id.file_name, R.id.icon, R.id.file_modify}
-        );
-
-        //填充数据集
-        lvFiles.setAdapter(adapter);
-        try{
-            tvPath.setText("当前路径为：" + currentParent.getCanonicalPath());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-    }
-*//*
-
-    */
-/*    private TextView tvPath;
-    private ListView lvFiles;
-    private Button btnParent;
-
-    //记录当前的父文件夹
-    File currentParent;
-
-    //记录当前路径下的所有文件夹的文件数组
-    File[] currentFiles;
-
-    File root;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sdcard_file_explorer);
-        lvFiles = this.findViewById(R.id.files);
-        tvPath = this.findViewById(R.id.tv_path);
-        btnParent = this.findViewById(R.id.btnParent);
-
-        //获取系统的SDCard的目录
-        try {
-             root = new File("/sdcard/Movies");
-        } catch (Exception e) {
-            e.printStackTrace();
-            Log.e("SDCard", "Error:"+e.toString());
-        }
-
-        //如果SD卡存在的话
-        if(root.exists()) {
-            currentParent = root;
-            Log.i("SDCard", root.toString());
-            try {
-                currentFiles = root.listFiles();
-            } catch (Exception e) {
-                e.printStackTrace();
-                Log.e("SDCard", "Error:"+e.toString());
-            }
-            //使用当前目录下的全部文件、文件夹来填充listView
-            inflateListView(currentFiles);
-            Log.i("SDCard", "SD卡存在");
-        }
-
-        Log.i("SDCard", "SD卡不存在");
-
-        lvFiles.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
-                if (currentFiles[position].isFile()) {
-                    return;
-                }
-
-                //获取用户点击的文件夹下的所有文件
-                File[] tem = currentFiles[position].listFiles();
-                if (tem == null || tem.length == 0) {
-                    Toast.makeText(InstallGameActivity.this, "当前路径不可访问或者该路径下没有文件", Toast.LENGTH_LONG).show();
-                } else {
-                    //获取用户点击的列表项对应的文件夹，设置为挡墙的父文件夹
-                    currentParent = currentFiles[position];
-                    //保存当前的父文件夹内的全部文件和文件夹
-                    currentFiles = tem;
-                    //再次更新ListView
-                    inflateListView(currentFiles);
-                }
-            }
-        });
-
-        //获取上一级目录
-        btnParent.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                try {
-                    if (!currentParent.getCanonicalPath().equals("/mnt/sdcard")) {
-                        //获取上一级目录
-                        currentParent = currentParent.getParentFile();
-                        //列出当前目录下的所有文件
-                        currentFiles = currentParent.listFiles();
-                        //再次更新ListView
-                        inflateListView(currentFiles);
-                    }
-                } catch (Exception e) {
-                    //TODO: handle exception
-                }
-            }
-        });
-    }
-
-
-
-
-    *//*
-*/
-/**
- * 根据文件夹填充ListView
- * @param files
- *//*
-*/
-/*
-    private void inflateListView(File[] files) {
-        Log.i("SDCard", "11111111");
-        if (files == null) {
-            Log.i("SDCard", "files is null");
-
-        }
-        Log.i("SDCard", "files.length:"+files.length);
-
-        List<Map<String, Object>> listItems = new ArrayList<Map<String, Object>>();
-        for (int i=0; i< files.length; i++) {
-            Map<String, Object> listItem = new HashMap<String, Object>();
-            if (files[i].isDirectory()) {
-                //如果是文件夹就显示图片为文件夹的图片
-                listItem.put("icon", R.drawable.folder);
-            } else {
-                listItem.put("icon", R.drawable.file);
-            }
-            Log.i("SDCard", "2222222");
-
-
-            //添加一个文件名称
-            listItem.put("filename", files[i].getName());
-            File myFile = new File(files[i].getName());
-
-            //获取文件的最后修改日期
-            long modTime = myFile.lastModified();
-            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-d HH:mm:ss");
-            System.out.println(dateFormat.format(new Date(modTime)));
-
-            //添加一个最后修改日期
-            listItem.put("modify", "修改日期："+dateFormat.format(new Date(modTime)));
-            listItems.add(listItem);
-        }
-        Log.i("SDCard", "33333333");
-
-        //定义一个SimpleAdapter
-        SimpleAdapter adapter = new SimpleAdapter(
-                InstallGameActivity.this, listItems, R.layout.list_item,
-                new String[]{"filename", "icon", "modify"}, new int[] {R.id.file_name, R.id.icon, R.id.file_modify}
-        );
-
-        //填充数据集
-        lvFiles.setAdapter(adapter);
-        try{
-            tvPath.setText("当前路径为：" + currentParent.getCanonicalPath());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-
-    }
-
-    *//*
-*/
 /**
  * 展现所有的Apk
  *//*
@@ -390,5 +175,4 @@ public class InstallGameActivity extends AppCompatActivity {
 
     }*/
 
-    }
 }
--- app/src/main/java/com/example/seafooler/hospitalapp/common/GameAdpater.java
+++ app/src/main/java/com/example/seafooler/hospitalapp/common/GameAdpater.java
@@ -2,15 +2,19 @@ package com.example.seafooler.hospitalapp.common;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.seafooler.hospitalapp.R;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by seafooler on 17-12-6.
@@ -19,15 +23,21 @@ import java.util.List;
 public class GameAdpater extends BaseAdapter {
     private Context mContext;
 
-    private Bitmap[] bms;
+    private Map<String, Bitmap> bms;
 
-    public GameAdpater(Context c, Bitmap[] bms) {
+    private ArrayList<String> apkNames = new ArrayList<>();
+
+    public GameAdpater(Context c, Map<String,Bitmap> bms) {
         mContext = c;
         this.bms = bms;
+        // Extract all the apk names
+        for (String key : bms.keySet()) {
+            apkNames.add(key);
+        }
     }
 
     public int getCount() {
-        return mThumbIds.length;
+        return apkNames.size();
     }
 
     public Object getItem(int position) {
@@ -40,7 +50,7 @@ public class GameAdpater extends BaseAdapter {
 
     // create a new ImageView for each item referenced by the Adpater
     public View getView(int position, View convertView, ViewGroup parent) {
-        ImageView imageView;
+        /*ImageView imageView;
         if (convertView == null) {
             //if it's not recycled, initialize some attributes
             imageView = new ImageView(mContext);
@@ -52,12 +62,29 @@ public class GameAdpater extends BaseAdapter {
         }
 
 //        imageView.setImageResource(mThumbIds[position]);
+        imageView.setImageBitmap();
         imageView.setImageBitmap(bms[position]);
-        return imageView;
+        return imageView;*/
+
+        final String apkName = apkNames.get(position);
+
+        if (convertView == null) {
+            final LayoutInflater layoutInflater = LayoutInflater.from(mContext);
+            convertView = layoutInflater.inflate(R.layout.game_unit, null);
+        }
+
+        final ImageView ivApkCover = convertView.findViewById(R.id.apk_cover);
+        final TextView tvApkName = convertView.findViewById(R.id.apk_name);
+
+        ivApkCover.setImageBitmap(bms.get(apkName));
+        tvApkName.setText(apkName);
+
+        return convertView;
+
     }
 
     //references to our images
-    private Integer[] mThumbIds = {
+    /*private Integer[] mThumbIds = {
             R.drawable.game, R.drawable.game,
             R.drawable.game, R.drawable.game,
             R.drawable.game, R.drawable.game,
@@ -69,6 +96,6 @@ public class GameAdpater extends BaseAdapter {
             R.drawable.game, R.drawable.game,
             R.drawable.game, R.drawable.game,
             R.drawable.game, R.drawable.game
-    };
+    };*/
 
 }
