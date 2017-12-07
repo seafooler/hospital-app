1587ecadc00b4bb2828adda991dadbe570948a00
seafooler <seafooler@gmail.com>
2017-12-07 21:46:11 +0800

    A tmp commit before leaving the lab

--- app/src/main/java/com/example/seafooler/hospitalapp/MainActivity.java
+++ app/src/main/java/com/example/seafooler/hospitalapp/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.seafooler.hospitalapp;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,6 +22,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //隐藏状态栏
+        Window window = getWindow();
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //隐藏标题栏
+//      getSupportActionBar().hide();
+
         setContentView(R.layout.activity_main);
         ivGame = findViewById(R.id.game);
         ivVideo = findViewById(R.id.video);
@@ -37,5 +46,17 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+
+    @Override
+    protected void onResume() {
+        /**
+         * 设定为横屏
+         */
+        if(getRequestedOrientation()!= ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE){
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        }
+        super.onResume();
     }
 }
